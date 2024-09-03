5507baa9ff7fc56393c48682aba802077311a04e
p1xta <151988951+p1xta@users.noreply.github.com>
2024-09-03 12:33:17 +0700

    Add files via upload

--- bubble_sort.py
+++ bubble_sort.py
@@ -11,7 +11,7 @@ def BubbleSort(N: int) -> list[int]:
     return lst
 
 parser = argparse.ArgumentParser()
-parser.add_argument('n', type=int)
+parser.add_argument('num', type=int)
 args = parser.parse_args()
-print(BubbleSort(int(args.n)))
+print(BubbleSort(int(args.num)))
 
