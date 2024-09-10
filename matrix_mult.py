602214ce67ac777540a6f3653a4b2ae480c2aaf6
p1xta <151988951+p1xta@users.noreply.github.com>
2024-09-10 11:01:17 +0700

    Add files via upload

--- matrix_mult.py
+++ matrix_mult.py
@@ -9,6 +9,16 @@ def read_matrix(file, matrix):
         nums = [int(num) for num in nums]
         matrix.append(nums)
 
+def check_matrix(file, matrix):
+    prev_row_lenght = len(matrix[0])
+    for row in matrix:
+        if len(row) != prev_row_lenght:
+            file.write("Matrices are invalid")
+            return False
+        else:
+            prev_row_lenght = len(row)
+    return True
+
 def multiply_matrices(matrix1, matrix2):
     result = [[0 for i in range(len(matrix2[0]))] for j in range(len(matrix1))]
     for i in range(len(matrix1)):
@@ -31,12 +41,17 @@ matrix_2 = []
 read_matrix(fin, matrix_1)
 read_matrix(fin, matrix_2)
 
-res = multiply_matrices(matrix_1, matrix_2)
+if (len(matrix_1[0]) != len(matrix_2)):
+    fout.write(f"Matrices can't be multipied ({len(matrix_1)}x{len(matrix_1[0])} and {len(matrix_2)}x{len(matrix_2[0])})")
+    exit(1)
 
-for i in range(len(res)):
-    for j in range(len(res[0])):
-        fout.write(f"{res[i][j]} ")
-    fout.write("\n")
+if check_matrix(fout, matrix_1) and check_matrix(fout, matrix_2):
+    res = multiply_matrices(matrix_1, matrix_2)
+
+    for i in range(len(res)):
+        for j in range(len(res[0])):
+            fout.write(f"{res[i][j]} ")
+        fout.write("\n")
 
 fin.close()
 fout.close()
